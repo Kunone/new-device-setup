08b66514b1971ebb64c82e088c1b6815fba2de9f
Kun Wang <kun.w@hotmail.com>
2022-07-21 23:18:35 +1200

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,27 @@
+# firefox developer
+# https://www.mozilla.org/en-US/firefox/developer/
+# https://www.mozilla.org/en-US/firefox/new/
+
+# 1password
+# spotify
+
+# iterm2
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# power10k level
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+# ssh
+
+# z
+# autocomplete
+
+# visual code
+# https://code.visualstudio.com/download
+
